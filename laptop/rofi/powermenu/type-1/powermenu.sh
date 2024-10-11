1d3f46c7c61520e743f161591ccf0e34887155ec
John Ling <johnlingbusiness@gmail.com>
2024-10-11 13:58:58 +1100

    Add files via upload
    Updated dotfiles for laptop

--- laptop/rofi/powermenu/type-1/powermenu.sh
+++ laptop/rofi/powermenu/type-1/powermenu.sh
@@ -29,7 +29,6 @@ no='No'
 # Rofi CMD
 rofi_cmd() {
 	rofi -dmenu \
-		-p "$host" \
 		-mesg "Uptime: $uptime" \
 		-theme ${dir}/${theme}.rasi
 }
@@ -54,7 +53,7 @@ confirm_exit() {
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+	echo -e "$lock\n\n$logout\n$reboot\n$shutdown" | rofi_cmd
 }
 
 # Execute Command
@@ -70,15 +69,7 @@ run_cmd() {
 			amixer set Master mute
 			systemctl suspend
 		elif [[ $1 == '--logout' ]]; then
-			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
-				i3-msg exit
-			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
-				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-			fi
+			killall -9 Hyprland
 		elif [[ $1 == '--lock' ]]; then
 			swaylock
 		fi
@@ -98,9 +89,6 @@ case ${chosen} in
         ;;
     $lock)
 		lock.sh
-        ;;
-    $suspend)
-		run_cmd --suspend
         ;;
     $logout)
 		run_cmd --logout
