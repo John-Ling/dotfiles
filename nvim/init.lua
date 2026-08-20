43e2b197bbf9d9be89f93ef6575a810cb92a08ec
John Ling <johnlingbusiness@gmail.com>
2026-08-21 09:22:02 +1000

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,12 @@
 require("config.lazy")
 
 
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = false
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.incsearch = true
+vim.cmd("colorscheme default")
 vim.opt.clipboard = "unnamedplus"
-
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlights' })
